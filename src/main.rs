decfc01869226023e02390ddbcadb9d352399dc7
Leo Spratt <enchant97@users.noreply.github.com>
2023-03-19 17:07:28 +0000

    create dummy pages

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use leptos::*;
+use leptos_router::*;
 
 pub(crate) mod contexts;
+pub(crate) mod pages;
 
 use contexts::{api::CurrentApi, login::CurrentLogin, toasts::Toasts};
+use pages::*;
 
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
@@ -17,6 +20,17 @@ pub fn App(cx: Scope) -> impl IntoView {
     provide_context(cx, Toasts::new(cx));
 
     view! { cx,
-        <h1 class="p-4">"Hello World!"</h1>
+        <Router>
+            <Routes>
+                <Route path="/" view=move |cx| view! {cx, <Home/>} />
+                <Route path="/signup" view=move |cx| view! {cx, <Signup/>} />
+                <Route path="/login" view=move |cx| view! {cx, <Login/>} />
+                <Route path="/logout" view=move |cx| view! {cx, <Logout/>} />
+                <Route path="/recipes" view=move |cx| view! {cx, <Recipes/>} />
+                <Route path="/recipes/new" view=move |cx| view! {cx, <NewRecipe/>} />
+                <Route path="/recipes/:id" view=move |cx| view! {cx, <Recipe/>} />
+                <Route path="/recipes/:id/print" view=move |cx| view! {cx, <RecipePrint/>} />
+            </Routes>
+        </Router>
     }
 }
---
+++ src/pages.rs
@@ -0,0 +1,41 @@
+use leptos::*;
+
+#[component]
+pub fn Home(cx: Scope) -> impl IntoView {
+    view! {cx, <h1>"Home"</h1>}
+}
+
+#[component]
+pub fn Signup(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn Login(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn Logout(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn Recipes(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn Recipe(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn RecipePrint(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
+
+#[component]
+pub fn NewRecipe(cx: Scope) -> impl IntoView {
+    view! {cx, <></>}
+}
