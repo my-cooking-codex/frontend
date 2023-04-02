b6265f992016b3f156503ad0469bb53d4f9d6218
Leo Spratt <enchant97@users.noreply.github.com>
2023-04-02 18:07:01 +0100

    handle errors on home page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -25,13 +25,17 @@ pub fn Home(cx: Scope) -> impl IntoView {
         cx,
         || {},
         move |_| async move {
-            match api.get().unwrap().get_stats().await {
-                Ok(stats) => Some(stats),
-                Err(err) => {
-                    toasts.push(api_error_to_toast(&err, "loading stats"));
-                    logout_on_401(&set_login, &err);
-                    None
+            if let Some(api) = api.get() {
+                match api.get_stats().await {
+                    Ok(stats) => Some(stats),
+                    Err(err) => {
+                        toasts.push(api_error_to_toast(&err, "loading stats"));
+                        logout_on_401(&set_login, &err);
+                        None
+                    }
                 }
+            } else {
+                None
             }
         },
     );
@@ -43,19 +47,17 @@ pub fn Home(cx: Scope) -> impl IntoView {
             <div class="p-4 rounded bg-base-200">
                 <h1 class="text-3xl font-bold mb-2">"Home"</h1>
                 <h2 class="text-2xl mb-2">"Your Stats"</h2>
-                <Suspense fallback=move || view!{cx, <></>}>
-                    {move || {
-                        account_stats.read(cx).map(|v| {
-                            let stats = v.map_or_else(|| AccountStats{ user_count: 0, recipe_count: 0 }, |v| v);
-                            let stats = vec![
-                                Stat::new("Number Of Users", &stats.user_count.to_string(), None),
-                                Stat::new("Number Of Recipes", &stats.recipe_count.to_string(), None),
-                                Stat::new("Number Of Books", "0", None),
-                            ];
-                            view!{cx, <Stats stats={stats}/>
-                        }})
-                    }}
-                </Suspense>
+                {move || {
+                    account_stats.read(cx).map(|v| {
+                        let stats = v.map_or_else(|| AccountStats{ user_count: 0, recipe_count: 0 }, |v| v);
+                        let stats = vec![
+                            Stat::new("Number Of Users", &stats.user_count.to_string(), None),
+                            Stat::new("Number Of Recipes", &stats.recipe_count.to_string(), None),
+                            Stat::new("Number Of Books", "0", None),
+                        ];
+                        view!{cx, <Stats stats={stats}/>
+                    }})
+                }}
             </div>
         </Drawer>
     }
