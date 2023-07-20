78e6e4e564eb4028cc77b1a5d7fbd69f9e3e2437
Leo Spratt <enchant97@users.noreply.github.com>
2023-07-20 11:05:26 +0100

    add custom theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
     content: [
-      "./src/**/*.rs"
+        "./src/**/*.rs"
     ],
     theme: {
-      extend: {},
+        extend: {},
     },
     plugins: [
-      require("daisyui"),
+        require("daisyui"),
     ],
-  }
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+                    "primary": "#2a76b7",
+                },
+                dark: {
+                    ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+                    "primary": "#235886",
+                },
+            },
+        ],
+    },
+}
